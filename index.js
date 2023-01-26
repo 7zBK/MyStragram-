75c9d4d85c6648c2350e83e7405371884c50224d
7zBK <PrempreeEz@gmail.com>
2023-01-26 16:50:14 +0700

    up

---
+++ index.js
@@ -0,0 +1,31 @@
+function appendImageElement(keyword, index){
+    const imgElement = document.createElement("img");
+    imgElement.className = "img" + index;
+    imgElement.src = `https://source.unsplash.com/225x225/?${keyword}&sig=${index}`;
+    const galleryElement = document.querySelector(".gallery");
+    galleryElement.appendChild(imgElement);
+}
+
+function removeAllImage(){
+    const galleryElement = document.querySelector(".gallery");
+    galleryElement.innerHTML = "";
+}
+
+function searchPhotos(event){
+    const keyword = event.target.value;
+    if(event.key === "Enter" && keyword){
+        removeAllImage();
+        for(let i = 1; i<=20; i++){
+            appendImageElement(keyword,i);
+        }
+    }
+}
+
+
+
+function main(){
+    // const inputElement = document.getElementsByClassName("search");
+    const inputElement = document.querySelector(".search");
+    inputElement.addEventListener("keydown",searchPhotos);
+}
+main();
